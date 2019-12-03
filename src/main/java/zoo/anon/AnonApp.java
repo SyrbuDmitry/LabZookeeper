4505a167dc6b742e7644043a0424f209df140f3c
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-03 22:48:07 +0300

    Scripted auto-commit on change (2019-12-03 22:48:07) by gitwatch.sh

--- src/main/java/zoo/anon/AnonApp.java
+++ src/main/java/zoo/anon/AnonApp.java
@@ -11,6 +11,5 @@ public class AnonApp {
         ActorSystem system = ActorSystem.create("lab5");
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         ActorRef configActor = system.actorOf(Props.create(ConfigStorageActor.class));
-        ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181",3000,this);
-    }
+   }
 }
--- src/main/java/zoo/anon/ConfigStorageActor.java
+++ src/main/java/zoo/anon/ConfigStorageActor.java
@@ -5,8 +5,12 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.japi.pf.ReceiveBuilder;
 import akka.stream.ActorMaterializer;
+import org.apache.zookeeper.ZooKeeper;
 
 public class ConfigStorageActor extends AbstractActor {
+    private void createZoo() throws Exception{
+        ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181",3000,this);
+    }
     @Override
     public Receive createReceive(){
         return ReceiveBuilder.create()
