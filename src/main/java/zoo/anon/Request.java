5a4089fdce72a422f03b402b6ff43c1da4e88dd6
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-03 22:14:10 +0300

    Scripted auto-commit on change (2019-12-03 22:14:10) by gitwatch.sh

--- src/main/java/zoo/anon/Request.java
+++ src/main/java/zoo/anon/Request.java
@@ -3,4 +3,10 @@ package zoo.anon;
 public class Request {
     public String url;
     public int count;
+    public int getCount(){
+        return count;
+    }
+    public String getUrl(){
+        return url;
+    }
 }
