1def52c11a0c26c876a3844a12ed20a8198746f3
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-03 23:39:56 +0300

    Scripted auto-commit on change (2019-12-03 23:39:56) by gitwatch.sh

--- src/main/java/zoo/anon/AnonApp.java
+++ src/main/java/zoo/anon/AnonApp.java
@@ -21,7 +21,7 @@ public class AnonApp {
         String path = zoo.create("/s",
                 "data".getBytes(),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
-        System.out.println(zoo.getChildren("/s", zooWatcher));
+        System.out.println(zoo.getChildren("/", zooWatcher));
 
     }
 }
