b481bf9af7afa8aa4374abe5ee8424ccfe735615
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-03 23:59:00 +0300

    Scripted auto-commit on change (2019-12-03 23:59:00) by gitwatch.sh

--- src/main/java/zoo/anon/AnonApp.java
+++ src/main/java/zoo/anon/AnonApp.java
@@ -18,15 +18,11 @@ public class AnonApp {
 //        final ActorMaterializer materializer = ActorMaterializer.create(system);
         ZooWatcher zooWatcher = new ZooWatcher();
         ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181", 3000,zooWatcher);
-        String path = zoo.create("/servers",
+        String path = zoo.create("/servers/s",
                 "data".getBytes(),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE,
                 CreateMode.EPHEMERAL_SEQUENTIAL);
         System.out.println(path);
-        zoo.create(path+"/s",
-                "data".getBytes(),
-                ZooDefs.Ids.OPEN_ACL_UNSAFE,
-                CreateMode.EPHEMERAL_SEQUENTIAL);
         List<String> servers = zoo.getChildren("/", zooWatcher);
         for (String s : servers) {
             byte[] data = zoo.getData("/" + s, false, null);
