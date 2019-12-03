cded9a89ff1521f03c94ce69ebc35bd0f64e502a
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-03 22:34:35 +0300

    Scripted auto-commit on change (2019-12-03 22:34:35) by gitwatch.sh

--- src/main/java/zoo/anon/AnonApp.java
+++ src/main/java/zoo/anon/AnonApp.java
@@ -1,7 +1,12 @@
 package zoo.anon;
 
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+
 public class AnonApp {
     public static void main(String[] args ){
-
+        ActorSystem system = ActorSystem.create("lab5");
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        
     }
 }
--- src/main/java/zoo/anon/ConfigStorageActor.java
+++ src/main/java/zoo/anon/ConfigStorageActor.java
@@ -2,7 +2,20 @@ package zoo.anon;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.japi.pf.ReceiveBuilder;
+import akka.stream.ActorMaterializer;
 
 public class ConfifStorageActor extends AbstractActor {
-    private ActorRef
+    private ActorSystem system;
+    private ActorMaterializer materializer;
+    ConfifStorageActor(ActorSystem system, ActorMaterializer materializer){
+        this.system = system;
+        this.materializer = materializer;
+    }
+    @Override
+    public Receive createReceive(){
+        return ReceiveBuilder.create()
+                .build();
+    }
 }
