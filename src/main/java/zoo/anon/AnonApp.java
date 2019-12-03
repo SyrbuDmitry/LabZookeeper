96329be5f5699fe9e80670a363493b883e6403cd
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-03 22:34:55 +0300

    Scripted auto-commit on change (2019-12-03 22:34:55) by gitwatch.sh

--- src/main/java/zoo/anon/AnonApp.java
+++ src/main/java/zoo/anon/AnonApp.java
@@ -7,6 +7,6 @@ public class AnonApp {
     public static void main(String[] args ){
         ActorSystem system = ActorSystem.create("lab5");
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        
+
     }
 }
