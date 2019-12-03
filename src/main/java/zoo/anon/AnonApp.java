6e327f2ac7d8699c92bb03713baee4d06123a43f
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-03 22:52:18 +0300

    Scripted auto-commit on change (2019-12-03 22:52:18) by gitwatch.sh

--- src/main/java/zoo/anon/AnonApp.java
+++ src/main/java/zoo/anon/AnonApp.java
@@ -4,12 +4,14 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.stream.ActorMaterializer;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 public class AnonApp {
     public static void main(String[] args ){
         ActorSystem system = ActorSystem.create("lab5");
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         ActorRef configActor = system.actorOf(Props.create(ConfigStorageActor.class));
-   }
+        ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181",3000,this);
+
+    }
 }
--- src/main/java/zoo/anon/ConfigStorageActor.java
+++ src/main/java/zoo/anon/ConfigStorageActor.java
@@ -5,15 +5,16 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.japi.pf.ReceiveBuilder;
 import akka.stream.ActorMaterializer;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
-public class ConfigStorageActor extends AbstractActor {
-    private void createZoo() throws Exception{
-        ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181",3000,this);
-    }
+public static class ConfigStorageActor implements Watcher {
     @Override
-    public Receive createReceive(){
-        return ReceiveBuilder.create()
-                .build();
+//    public Receive createReceive(){
+//        return ReceiveBuilder.create()
+//                .build();
+    public void process(WatchedEvent event){
+
     }
 }
