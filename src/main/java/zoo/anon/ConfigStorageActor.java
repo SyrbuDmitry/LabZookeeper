89cfa5de836f81ccce6ab2654344a75235660a1a
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-03 22:59:01 +0300

    Scripted auto-commit on change (2019-12-03 22:59:01) by gitwatch.sh

--- src/main/java/zoo/anon/ConfigStorageActor.java
+++ src/main/java/zoo/anon/ConfigStorageActor.java
@@ -5,16 +5,20 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.japi.pf.ReceiveBuilder;
 import akka.stream.ActorMaterializer;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
-public static class ConfigStorageActor implements Watcher {
-    @Override
-//    public Receive createReceive(){
-//        return ReceiveBuilder.create()
-//                .build();
-    public void process(WatchedEvent event){
+import java.io.IOException;
 
+public class ConfigStorageActor extends AbstractActor {
+    private ZooWatcher zooWatcher;
+    private ZooKeeper zoo;
+    ConfigStorageActor() throws IOException{
+        zooWatcher = new ZooWatcher();
+        zoo = new ZooKeeper("127.0.0.1:2181",3000,zooWatcher);
+    }
+    @Override
+    public Receive createReceive(){
+        return ReceiveBuilder.create()
+                .build();
     }
 }
--- src/main/java/zoo/anon/ZooWatcher.java
+++ src/main/java/zoo/anon/ZooWatcher.java
@@ -1,4 +1,11 @@
 package zoo.anon;
 
-public class ZooWatcher {
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+public class ZooWatcher implements Watcher {
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
 }
