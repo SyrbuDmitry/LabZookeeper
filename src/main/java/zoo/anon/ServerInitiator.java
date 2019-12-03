816029c628ae8d09d74a1bcb8c47e9683267e9c1
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-04 00:51:53 +0300

    Scripted auto-commit on change (2019-12-04 00:51:53) by gitwatch.sh

--- src/main/java/zoo/anon/ServerInitiator.java
+++ src/main/java/zoo/anon/ServerInitiator.java
@@ -1,9 +1,16 @@
 package zoo.anon;
 
+import akka.actor.ActorRef;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
+import scala.concurrent.Future;
+
+import static akka.http.javadsl.server.Directives.*;
 
 public class ServerInitiator {
     private ZooKeeper zoo;
@@ -16,5 +23,18 @@ public class ServerInitiator {
                 ZooDefs.Ids.OPEN_ACL_UNSAFE,
                 CreateMode.EPHEMERAL);
         System.out.println(path+" Connected");
+    }
+    private Route createRoute() {
+        return
+                route(
+                        pathSingleSlash(() ->
+                                get(() ->
+                                        parameter("packageID", id -> {
+                                                    Future<Object> result = Patterns.ask(RouteActor, new GetResultMessage(Integer.parseInt(id)), 5000);
+                                                    return completeOKWithFuture(result, Jackson.marshaller());
+                                                }
+                                        )
+                                )
+                        );
     }
 }
