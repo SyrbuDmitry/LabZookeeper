e627c470f5b460d77087045c58c1fee84acfc5e6
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-03 22:42:13 +0300

    Scripted auto-commit on change (2019-12-03 22:42:13) by gitwatch.sh

--- src/main/java/zoo/anon/AnonApp.java
+++ src/main/java/zoo/anon/AnonApp.java
@@ -4,12 +4,12 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.stream.ActorMaterializer;
-import org.apache.zookeeper;
+import org.apache.zookeeper.*;
 public class AnonApp {
     public static void main(String[] args ){
         ActorSystem system = ActorSystem.create("lab5");
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         ActorRef configActor = system.actorOf(Props.create(ConfigStorageActor.class));
-        ZooKeeper
+        Zook
     }
 }
