b3774c10eba80a80628c1e6451b2e5f7dd9bd3a4
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-03 23:20:37 +0300

    Scripted auto-commit on change (2019-12-03 23:20:37) by gitwatch.sh

--- src/main/java/zoo/anon/AnonApp.java
+++ src/main/java/zoo/anon/AnonApp.java
@@ -4,13 +4,24 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.stream.ActorMaterializer;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.io.IOException;
+import java.util.List;
+
 public class AnonApp {
-    public static void main(String[] args ){
+    public static void main(String[] args ) throws InterruptedException,IOException,KeeperException{
         ActorSystem system = ActorSystem.create("lab6");
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        ActorRef configActor = system.actorOf(Props.create(ConfigStorageActor.class));
+        ZooWatcher zooWatcher = new ZooWatcher();
+        ZooKeeper zoo = new ZooKeeper("127.0.0.1:2181", 3000,zooWatcher);
+        zoo.create("/servers/s",
+                "data".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        List<String> servers = zoo.getChildren("/servers", zooWatcher);
 
 
     }
