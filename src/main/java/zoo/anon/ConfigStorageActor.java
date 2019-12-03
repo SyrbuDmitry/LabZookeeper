d40fb6c67f2ab9389d694f7f46af2cefad365a8c
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-03 23:13:45 +0300

    Scripted auto-commit on change (2019-12-03 23:13:45) by gitwatch.sh

--- src/main/java/zoo/anon/ConfigStorageActor.java
+++ src/main/java/zoo/anon/ConfigStorageActor.java
@@ -18,7 +18,7 @@ public class ConfigStorageActor extends AbstractActor {
     private ZooKeeper zoo;
     ConfigStorageActor() throws IOException,KeeperException,InterruptedException {
         zooWatcher = new ZooWatcher();
-        zoo = new ZooKeeper("127.0.0.1:2181", 3000, zooWatcher);
+        zoo = new ZooKeeper("127.0.0.1:2181", 3000,zooWatcher);
         zoo.create("/servers/s",
                 "data".getBytes(),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
