69b1a3969d87e9a1e5b8f6d9ab00856a08826bc6
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-05 17:13:46 +0300

    Scripted auto-commit on change (2019-12-05 17:13:46) by gitwatch.sh

--- src/main/java/zoo/anon/ServerInitiator.java
+++ src/main/java/zoo/anon/ServerInitiator.java
@@ -53,7 +53,7 @@ public class ServerInitiator implements Watcher{
                         pathSingleSlash(() ->
                                 parameter("url", (url) ->
                                         parameter("count", (count) ->{
-                                           
+
                                             return  handleRequest(new Request(url,count));
                                         })
                                 )
