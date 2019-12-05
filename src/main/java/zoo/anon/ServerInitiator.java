9895ad64c60a6469f86481a0a94b5b53831a3819
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-12-05 13:37:06 +0300

    Scripted auto-commit on change (2019-12-05 13:37:06) by gitwatch.sh

--- src/main/java/zoo/anon/ServerInitiator.java
+++ src/main/java/zoo/anon/ServerInitiator.java
@@ -47,7 +47,7 @@ public class ServerInitiator {
     }
 
     private Route handleRequest(Request r){
-        return r.count==0 ? completeOKWithFuture(fetch(r.url).toCompletableFuture().thenCompose :
+        return r.count==0 ? completeOKWithFuture(fetch(r.url).toCompletableFuture().thenApply(e -> e.entity())):
     }
 
     private CompletionStage<HttpResponse> fetch(String url) {
